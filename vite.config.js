8678a6d621ec34a4fba06d0fd4140fee1429341f
Saiful <ipoela@gmail.com>
2025-09-15 17:24:08 +0700

    Update Home Page Job Listing

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: '3.104.202.133', // <-- your server public IP
+        },
+    },
 });
